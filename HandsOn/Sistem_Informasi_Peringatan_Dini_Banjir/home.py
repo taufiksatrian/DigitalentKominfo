06e65525642166dcb0d2c52ca83e6edc0b5108ae
taufiksatrian <72427297+taufiksatrian@users.noreply.github.com>
2023-11-17 11:15:19 +0700

    Last

---
+++ HandsOn/Sistem_Informasi_Peringatan_Dini_Banjir/home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home",
+    page_icon="🌧️",
+)
+
+st.write("# Sistem Informasi Peringatan Dini Banjir")
+
+
+st.markdown(
+    """
+"""
+)
